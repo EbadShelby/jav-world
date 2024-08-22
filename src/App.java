f94497819f66de0e03122bc8bdc66e230b49dbb3
EbadShelby <ebadyasser123xd@gmail.com>
2024-08-22 16:38:49 +0800

    another one

--- src/App.java
+++ src/App.java
@@ -1,37 +1,38 @@
+import java.util.Random;
 import java.util.Scanner;
+import javax.swing.JOptionPane;
+import java.util.random.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // System.out.println("Hello, World!");
-        // int a = 9;
-        // float b = a;
-        // System.out.println(b);
-        // int z = (int) b;
-        // System.out.println(z);
+/*         System.out.println("Hello, World!");
+        int a = 9;
+        float b = a;
+        System.out.println(b);
+        int z = (int) b;
+        System.out.println(z); */
 
-        // int max = Math.max(10, 2);
-        // System.out.println("max: " + max);
-        // int min = Math.min(10, 2);
-        // System.out.println("min: " + min);
-        // double sqrt = Math.sqrt(64);
-        // System.out.println("sqrt root: " + sqrt);
-        // int abs = Math.abs(-5);
-        // System.out.println("absolute: " + abs);
-        // int random = (int) (Math.random() * 10);
-        // System.out.println("random number: " + random);
+/*         int max = Math.max(10, 2);
+        System.out.println("max: " + max);
+        int min = Math.min(10, 2);
+        System.out.println("min: " + min);
+        double sqrt = Math.sqrt(64);
+        System.out.println("sqrt root: " + sqrt);
+        int abs = Math.abs(-5);
+        System.out.println("absolute: " + abs);
+        int random = (int) (Math.random() * 10);
+        System.out.println("random number: " + random); */
 
+/*         String first = "joe";
+        String second = "jake";
+        String box;
+        box = first;
+        first = second;
+        second = box;
+        System.out.println(first);
+        System.out.println(second); */
 
-        // String first = "joe";
-        // String second = "jake";
-        
-        // String box;
-        // box = first;
-        // first = second;
-        // second = box;
-        // System.out.println(first);
-        // System.out.println(second);
-
-        @SuppressWarnings("resource")
+/*      @SuppressWarnings("resource")
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("What's your first name ?");
@@ -49,6 +50,77 @@ public class App {
 
         System.out.println("What's your fave food?");
         String food = scanner.nextLine();
-        System.out.println("that's also one of my favorite food!, "  + food);
+        System.out.println("that's also one of my favorite food!, " + food); */
+
+        /* double friends = 10;
+        friends = (double) friends / 3;
+        System.out.println(friends);
+
+        String name = JOptionPane.showInputDialog("Enter your house");
+        JOptionPane.showMessageDialog(null, "What's up " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your ages"));
+        JOptionPane.showMessageDialog(null, age +"?" +" you young!?"); */
+
+        /* HYPOTENUSE
+        System.out.println("###########################################");
+        System.out.println("######### Find out the hypotenuse #########");
+        System.out.println("###########################################");
+        System.out.println("");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter side x: ");
+        double x = scanner.nextDouble(); 
+        System.out.print("Enter side y: ");
+        double y = scanner.nextDouble(); 
+        double hypotenuse = Math.sqrt((x * x) + (y * y)); 
+        System.out.println(STR."the Hypotenuse of \{x} and \{y} is \{hypotenuse}"); */
+
+/*         Random random = new Random();
+
+        int x = random.nextInt(6)+1;
+        System.out.println(x); */
+
+/*         int age = 31;
+
+        if (age >= 18 && age <= 30) {
+            System.out.println("You adults");
+        }
+        else if(age <= 18) {
+            System.out.println("you young");
+        }
+        else {
+            System.out.println("you too olds");
+        } */
+
+        /* String day = "sunday";
+
+        switch(day) {
+            case "monday": System.out.println("monday too colds");
+            break;
+            case "tuesday": System.out.println("tuesday too colds");
+            break;
+            case "wednesday": System.out.println("wednesday too colds");
+            break;
+            case "thursday": System.out.println("thursday too colds");
+            break;
+            case "friday": System.out.println("friday too colds");
+            break;
+            case "saturday": System.out.println("saturday too colds");
+            break;
+            case "sunday": System.out.println("sunday too colds");
+            break;
+            default: System.out.println("not a day bro");
+        } */
+
+/*         Scanner scanner = new Scanner(System.in);
+        String name = "";
+        do {
+            System.out.println("Enter your name: ");
+            name = scanner.nextLine();
+        }while (name.isBlank());
+
+        System.out.println(STR."what's up \{name}"); */
+
+        
     }
 }
